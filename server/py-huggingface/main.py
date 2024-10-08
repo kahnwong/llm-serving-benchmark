c3aa3facaafbbc02bd67a778792679e4234a6eaa
Karn Wong <karn@karnwong.me>
2024-10-09 00:17:36 +0700

    add fastapi

--- server/py-huggingface/main.py
+++ server/py-huggingface/main.py
@@ -1,10 +1,14 @@
 import torch
 import transformers
+from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import AutoTokenizer
 
+app = FastAPI(title="py-huggingface")
 
 model = "TinyLlama/TinyLlama_v1.1"
 tokenizer = AutoTokenizer.from_pretrained(model)
+
 pipeline = transformers.pipeline(
     "text-generation",
     model=model,
@@ -12,14 +16,26 @@ pipeline = transformers.pipeline(
     device_map="auto",
 )
 
-sequences = pipeline(
-    "How many helicopters can a human eat in one sitting?",
-    do_sample=True,
-    top_k=10,
-    num_return_sequences=1,
-    repetition_penalty=1.5,
-    eos_token_id=tokenizer.eos_token_id,
-    max_length=500,
-)
-for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+
+class RequestItem(BaseModel):
+    role: str
+    content: str
+
+
+@app.post("/")
+async def main(request: RequestItem):
+    sequences = pipeline(
+        request.content,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        repetition_penalty=1.5,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=500,
+    )
+
+    response = ""
+    for seq in sequences:
+        response += seq["generated_text"]
+
+    return {"content": response}
