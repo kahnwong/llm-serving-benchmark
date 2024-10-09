b45583e3313ba77e81c6863abca5dd1806833555
Karn Wong <karn@karnwong.me>
2024-10-09 10:27:27 +0700

    WIP

--- server/rs-candle/src/main.rs
+++ server/rs-candle/src/main.rs
@@ -1,214 +1,215 @@
-mod candle_server_utils;
 mod token_output_stream;
+use anyhow::{Error as E, Result};
+use clap::Parser;
+mod candle_server_utils;
+use candle_transformers::models::gemma::{Config as Config1, Model as Model1};
 
-use anyhow::{bail, Error as E, Result};
-use clap::{Parser, ValueEnum};
-
-use candle_core::{DType, Tensor};
+use candle_core::{DType, Device, Tensor};
 use candle_nn::VarBuilder;
-use candle_transformers::generation::{LogitsProcessor, Sampling};
+use candle_transformers::generation::LogitsProcessor;
 use hf_hub::{api::sync::Api, Repo, RepoType};
-use std::io::Write;
+use token_output_stream::TokenOutputStream;
+use tokenizers::Tokenizer;
 
-use candle_transformers::models::llama as model;
-use model::{Llama, LlamaConfig};
 
-const EOS_TOKEN: &str = "</s>";
-const DEFAULT_PROMPT: &str = "My favorite theorem is ";
+struct TextGeneration {
+    model: Model1,
+    device: Device,
+    tokenizer: TokenOutputStream,
+    logits_processor: LogitsProcessor,
+    repeat_penalty: f32,
+    repeat_last_n: usize,
+}
 
-#[derive(Clone, Debug, Copy, PartialEq, Eq, ValueEnum)]
-enum Which {
-    #[value(name = "tiny-llama-1.1b-chat")]
-    TinyLlama1_1BChat,
+impl TextGeneration {
+    #[allow(clippy::too_many_arguments)]
+    fn new(
+        model: Model1,
+        tokenizer: Tokenizer,
+        seed: u64,
+        temp: Option<f64>,
+        top_p: Option<f64>,
+        repeat_penalty: f32,
+        repeat_last_n: usize,
+        device: &Device,
+    ) -> Self {
+        let logits_processor = LogitsProcessor::new(seed, temp, top_p);
+        Self {
+            model,
+            tokenizer: TokenOutputStream::new(tokenizer),
+            logits_processor,
+            repeat_penalty,
+            repeat_last_n,
+            device: device.clone(),
+        }
+    }
+
+    fn run(&mut self, prompt: &str, sample_len: usize) -> Result<()> {
+        use std::io::Write;
+        self.tokenizer.clear();
+        let mut tokens = self
+            .tokenizer
+            .tokenizer()
+            .encode(prompt, true)
+            .map_err(E::msg)?
+            .get_ids()
+            .to_vec();
+        for &t in tokens.iter() {
+            if let Some(t) = self.tokenizer.next_token(t)? {
+                print!("{t}")
+            }
+        }
+        std::io::stdout().flush()?;
+
+        let mut generated_tokens = 0usize;
+        let eos_token = match self.tokenizer.get_token("<eos>") {
+            Some(token) => token,
+            None => anyhow::bail!("cannot find the <eos> token"),
+        };
+        let start_gen = std::time::Instant::now();
+        for index in 0..sample_len {
+            let context_size = if index > 0 { 1 } else { tokens.len() };
+            let start_pos = tokens.len().saturating_sub(context_size);
+            let ctxt = &tokens[start_pos..];
+            let input = Tensor::new(ctxt, &self.device)?.unsqueeze(0)?;
+            let logits = self.model.forward(&input, start_pos)?;
+            let logits = logits.squeeze(0)?.squeeze(0)?.to_dtype(DType::F32)?;
+            let logits = if self.repeat_penalty == 1. {
+                logits
+            } else {
+                let start_at = tokens.len().saturating_sub(self.repeat_last_n);
+                candle_transformers::utils::apply_repeat_penalty(
+                    &logits,
+                    self.repeat_penalty,
+                    &tokens[start_at..],
+                )?
+            };
+
+            let next_token = self.logits_processor.sample(&logits)?;
+            tokens.push(next_token);
+            generated_tokens += 1;
+            if next_token == eos_token {
+                break;
+            }
+            if let Some(t) = self.tokenizer.next_token(next_token)? {
+                print!("{t}");
+                std::io::stdout().flush()?;
+            }
+        }
+        let dt = start_gen.elapsed();
+        if let Some(rest) = self.tokenizer.decode_rest().map_err(E::msg)? {
+            print!("{rest}");
+        }
+        std::io::stdout().flush()?;
+        println!(
+            "\n{generated_tokens} tokens generated ({:.2} token/s)",
+            generated_tokens as f64 / dt.as_secs_f64(),
+        );
+        Ok(())
+    }
 }
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Run on CPU rather than on GPU.
     #[arg(long)]
-    cpu: bool,
-
-    /// The temperature used to generate samples.
-    #[arg(long, default_value_t = 0.8)]
-    temperature: f64,
+    prompt: String,
 
     /// Nucleus sampling probability cutoff.
     #[arg(long)]
     top_p: Option<f64>,
-
-    /// Only sample among the top K samples.
-    #[arg(long)]
-    top_k: Option<usize>,
 
     /// The seed to use when generating random samples.
     #[arg(long, default_value_t = 299792458)]
     seed: u64,
 
     /// The length of the sample to generate (in tokens).
-    #[arg(short = 'n', long, default_value_t = 10000)]
+    #[arg(long, short = 'n', default_value_t = 10000)]
     sample_len: usize,
-
-    /// Disable the key-value cache.
-    #[arg(long)]
-    no_kv_cache: bool,
-
-    /// The initial prompt.
-    #[arg(long)]
-    prompt: Option<String>,
-
-    /// Use different dtype than f16
-    #[arg(long)]
-    dtype: Option<String>,
-
-    /// Enable tracing (generates a trace-timestamp.json file).
-    #[arg(long)]
-    tracing: bool,
 
     #[arg(long)]
     model_id: Option<String>,
 
     #[arg(long)]
-    revision: Option<String>,
-
-    /// The model size to use.
-    #[arg(long, default_value = "tiny-llama-1.1b-chat")]
-    which: Which,
+    config_file: Option<String>,
 
     #[arg(long)]
-    use_flash_attn: bool,
-
-    /// Penalty to be applied for repeating tokens, 1. means no penalty.
-    #[arg(long, default_value_t = 1.1)]
-    repeat_penalty: f32,
-
-    /// The context size to consider for the repeat penalty.
-    #[arg(long, default_value_t = 128)]
-    repeat_last_n: usize,
+    weight_files: Option<String>,
 }
 
 fn main() -> Result<()> {
-    use tokenizers::Tokenizer;
+    let args = Args::parse(); // [TODO] remove
 
-    let args = Args::parse();
-
-    let device = candle_server_utils::device(args.cpu)?;
-    let dtype = match args.dtype.as_deref() {
-        Some("f16") => DType::F16,
-        Some("bf16") => DType::BF16,
-        Some("f32") => DType::F32,
-        Some(dtype) => bail!("Unsupported dtype {dtype}"),
-        None => DType::F16,
-    };
-    let (llama, tokenizer_filename, mut cache, config) = {
-        let api = Api::new()?;
-        let model_id = args.model_id.unwrap_or_else(|| match args.which {
-            Which::TinyLlama1_1BChat => "TinyLlama/TinyLlama-1.1B-Chat-v1.0".to_string(),
-        });
-        println!("loading the model weights from {model_id}");
-        let revision = args.revision.unwrap_or("main".to_string());
-        let api = api.repo(Repo::with_revision(model_id, RepoType::Model, revision));
-
-        let tokenizer_filename = api.get("tokenizer.json")?;
-        let config_filename = api.get("config.json")?;
-        let config: LlamaConfig = serde_json::from_slice(&std::fs::read(config_filename)?)?;
-        let config = config.into_config(args.use_flash_attn);
-
-        let filenames = match args.which {
-             Which::TinyLlama1_1BChat => {
-                vec![api.get("model.safetensors")?]
-            }
-        };
-        let cache = model::Cache::new(!args.no_kv_cache, dtype, &config, &device)?;
-
-        let vb = unsafe { VarBuilder::from_mmaped_safetensors(&filenames, dtype, &device)? };
-        (Llama::load(vb, &config)?, tokenizer_filename, cache, config)
-    };
-    let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
-    let eos_token_id = config.eos_token_id.or_else(|| {
-        tokenizer
-            .token_to_id(EOS_TOKEN)
-            .map(model::LlamaEosToks::Single)
-    });
-    let prompt = args.prompt.as_ref().map_or(DEFAULT_PROMPT, |p| p.as_str());
-    let mut tokens = tokenizer
-        .encode(prompt, true)
-        .map_err(E::msg)?
-        .get_ids()
-        .to_vec();
-    let mut tokenizer = token_output_stream::TokenOutputStream::new(tokenizer);
-
-    println!("starting the inference loop");
-    print!("{prompt}");
-    let mut logits_processor = {
-        let temperature = args.temperature;
-        let sampling = if temperature <= 0. {
-            Sampling::ArgMax
-        } else {
-            match (args.top_k, args.top_p) {
-                (None, None) => Sampling::All { temperature },
-                (Some(k), None) => Sampling::TopK { k, temperature },
-                (None, Some(p)) => Sampling::TopP { p, temperature },
-                (Some(k), Some(p)) => Sampling::TopKThenTopP { k, p, temperature },
-            }
-        };
-        LogitsProcessor::from_sampling(args.seed, sampling)
-    };
-
-    let mut start_gen = std::time::Instant::now();
-    let mut index_pos = 0;
-    let mut token_generated = 0;
-    for index in 0..args.sample_len {
-        let (context_size, context_index) = if cache.use_kv_cache && index > 0 {
-            (1, index_pos)
-        } else {
-            (tokens.len(), 0)
-        };
-        if index == 1 {
-            start_gen = std::time::Instant::now()
-        }
-        let ctxt = &tokens[tokens.len().saturating_sub(context_size)..];
-        let input = Tensor::new(ctxt, &device)?.unsqueeze(0)?;
-        let logits = llama.forward(&input, context_index, &mut cache)?;
-        let logits = logits.squeeze(0)?;
-        let logits = if args.repeat_penalty == 1. {
-            logits
-        } else {
-            let start_at = tokens.len().saturating_sub(args.repeat_last_n);
-            candle_transformers::utils::apply_repeat_penalty(
-                &logits,
-                args.repeat_penalty,
-                &tokens[start_at..],
-            )?
-        };
-        index_pos += ctxt.len();
-
-        let next_token = logits_processor.sample(&logits)?;
-        token_generated += 1;
-        tokens.push(next_token);
-
-        match eos_token_id {
-            Some(model::LlamaEosToks::Single(eos_tok_id)) if next_token == eos_tok_id => {
-                break;
-            }
-            Some(model::LlamaEosToks::Multiple(ref eos_ids)) if eos_ids.contains(&next_token) => {
-                break;
-            }
-            _ => (),
-        }
-        if let Some(t) = tokenizer.next_token(next_token)? {
-            print!("{t}");
-            std::io::stdout().flush()?;
-        }
-    }
-    if let Some(rest) = tokenizer.decode_rest().map_err(E::msg)? {
-        print!("{rest}");
-    }
-    let dt = start_gen.elapsed();
+    // log accelerators
     println!(
-        "\n\n{} tokens generated ({} token/s)\n",
-        token_generated,
-        (token_generated - 1) as f64 / dt.as_secs_f64(),
+        "cuda: {}, metal: {}",
+        candle_core::utils::cuda_is_available(),
+        candle_core::utils::metal_is_available(),
     );
+
+    // log parameters
+    const TEMPERATURE: Option<f64> = Some(0.0);
+    /// Penalty to be applied for repeating tokens, 1. means no penalty.
+    const REPEAT_PENALTY: f32 = 1.1;
+    /// The context size to consider for the repeat penalty.
+    const REPEAT_LAST_N: usize = 64;
+
+    println!(
+        "temp: {:.2} repeat-penalty: {:.2} repeat-last-n: {}",
+        TEMPERATURE.unwrap(),
+        REPEAT_PENALTY,
+        REPEAT_LAST_N
+    );
+
+    // ------------ get assets ------------
+    let start = std::time::Instant::now();
+
+    let api = Api::new()?;
+    let model_id = "google/gemma-1.1-2b-it".to_string();
+    let repo = api.repo(Repo::with_revision(
+        model_id,
+        RepoType::Model,
+        "main".to_string(),
+    ));
+
+    let tokenizer_filename = repo.get("tokenizer.json")?;
+    let config_filename = repo.get("config.json")?;
+
+    let filenames =
+        candle_server_utils::hub_load_safetensors(&repo, "model.safetensors.index.json")?;
+    println!("retrieved the files in {:?}", start.elapsed());
+
+    // ------------ init model ------------
+    let tokenizer = Tokenizer::from_file(tokenizer_filename).map_err(E::msg)?;
+
+    let start = std::time::Instant::now();
+
+    const IS_USE_CPU: bool = false;
+    let device = candle_server_utils::device(IS_USE_CPU)?;
+    let dtype = if device.is_cuda() {
+        DType::BF16
+    } else {
+        DType::F32
+    };
+    let vb = unsafe { VarBuilder::from_mmaped_safetensors(&filenames, dtype, &device)? };
+
+
+    let config: Config1 = serde_json::from_reader(std::fs::File::open(config_filename)?)?;
+    let model = Model1::new(false, &config, vb)?;
+
+    println!("loaded the model in {:?}", start.elapsed());
+
+    // ------------ inference ------------
+    let mut pipeline = TextGeneration::new(
+        model,
+        tokenizer,
+        args.seed,
+        TEMPERATURE,
+        args.top_p,
+        REPEAT_PENALTY,
+        REPEAT_LAST_N,
+        &device,
+    );
+    pipeline.run(&args.prompt, args.sample_len)?;
     Ok(())
 }
